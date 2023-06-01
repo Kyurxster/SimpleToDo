0aa5cd39a9ca639fb5f51ce947e1350c1cb6bfe2
Shannon <22020995@myrp.edu.sg>
2023-06-01 12:35:05 +0800

    Added OnClickListener and function for add button

--- app/src/main/java/sg/edu/rp/c346/id22020995/simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22020995/simpletodo/MainActivity.java
@@ -3,9 +3,13 @@ package sg.edu.rp.c346.id22020995.simpletodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnAdd;
     Button btnClear;
     ListView lvTasks;
+    ArrayList<String> alTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +28,22 @@ public class MainActivity extends AppCompatActivity {
         btnAdd = findViewById(R.id.buttonAdd);
         btnClear = findViewById(R.id.buttonClear);
         lvTasks = findViewById(R.id.listViewTasks);
+
+        alTasks = new ArrayList<String>();
+
+        // create and set ArrayAdapter for lvTasks
+        ArrayAdapter aaTasks = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alTasks);
+        lvTasks.setAdapter(aaTasks);
+
+        // set OnClickListener for btnAdd
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String taskInput = etTask.getText().toString();
+                alTasks.add(taskInput);
+                aaTasks.notifyDataSetChanged();
+                etTask.setText("");
+            }
+        });
     }
 }
