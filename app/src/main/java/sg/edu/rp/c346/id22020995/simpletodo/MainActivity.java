30483363f41fc8f63dc21ed6f2696cadf0ffeab8
Shannon <22020995@myrp.edu.sg>
2023-06-01 12:22:06 +0800

    Completed layout and assign variables to objects

--- app/src/main/java/sg/edu/rp/c346/id22020995/simpletodo/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/id22020995/simpletodo/MainActivity.java
@@ -3,12 +3,25 @@ package sg.edu.rp.c346.id22020995.simpletodo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etTask;
+    Button btnAdd;
+    Button btnClear;
+    ListView lvTasks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etTask = findViewById(R.id.editTextTask);
+        btnAdd = findViewById(R.id.buttonAdd);
+        btnClear = findViewById(R.id.buttonClear);
+        lvTasks = findViewById(R.id.listViewTasks);
     }
 }
